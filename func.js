05632aba90522378f48ae9c9317bc598a492bfd1
Oscarj-8 <mmmabdulahi954@yahoo.com>
2022-09-10 00:09:51 +0300

    some issues fixed

--- func.js
+++ func.js
@@ -25,3 +25,38 @@ function showCurrentStep() {
     step.classList.toggle("active", index === currentStep);
   });
 }
+
+var personal_info = document.getElementById("personal-info");
+var emergency_cont = document.getElementById("emergency-cont");
+var refernce_person = document.getElementById("refernce-person");
+var reasons_fr_vol = document.getElementById("reasons-fr-vol");
+
+var Next1 = document.getElementById("Next1");
+var Next2 = document.getElementById("Next2");
+var Next3 = document.getElementById("Next3");
+var Back1 = document.getElementById("Back1");
+var Back2 = document.getElementById("Back2");
+var Back3 = document.getElementById("Back3");
+
+Next1.onclick = function () {
+  progress.style.width = "450px";
+};
+Back1.onclick = function () {
+  progress.style.width = "225px";
+};
+
+Next2.onclick = function () {
+  progress.style.width = "675px";
+};
+
+Back2.onclick = function () {
+  progress.style.width = "450px";
+};
+
+Next3.onclick = function () {
+  progress.style.width = "1000px";
+};
+
+Back3.onclick = function () {
+  progress.style.width = "675px";
+};
